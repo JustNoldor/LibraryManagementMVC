e32560141aa2d2526309c4a71ad606136edbc864
Mert <mertsariboga@outlook.com>
2019-03-20 22:08:27 +0300

    .gitignore fix

---
+++ LibraryManagement/Areas/Panel/Controllers/PanelHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Areas.Panel.Controllers
+{
+    public class PanelHomeController : Controller
+    {
+        // GET: Panel/PanelHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryManagement/Areas/Panel/Controllers/PanelKategoriController.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Areas.Panel.Controllers
+{
+    public class PanelKategoriController : Controller
+    {
+
+        //Database Bağlantısı (Eng:Database Connection)
+        LibraryDBEntities _db = new LibraryDBEntities();
+
+        public ActionResult Index()
+        {
+            //Kategori Listeleme (Eng: Category Listing)
+            var listcategory = _db.Kategori.ToList();
+            return View(listcategory);
+        }
+
+
+
+
+    }
+}
